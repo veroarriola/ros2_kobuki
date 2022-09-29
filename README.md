92248bc5e736771384254176c7ee20d376ccba0e
Vero <v.arriola@ciencias.unam.mx>
2022-09-29 17:31:12 -0500

    ¡Ya se ve la Kobuki!

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # ros2_kobuki
-Nodo para simular la kobuki
+Paquetes para simular la kobuki
+
+## Prerrequisitos
+
+```
+$ sudo apt install ros-humble-urdf-tutorial
+```
+
+## Instalar
+
+Dentro de un espacio de trabajo, clonar este repo de modo que los directorios queden dentro de ```src``` y compilar con ```colcon```.
+
+```
+$ git clone <repo> .
+```
+
+## Ejecutar
+
+```
+$ ros2 launch kobuki_description view_model.py
+```
--- kobuki_description/launch/view_model.py
+++ kobuki_description/launch/view_model.py
@@ -1,4 +1,3 @@
-# from lauch_ros.substitutions import FindPackageShare, PathJoinSubstitution
 import os
 from ament_index_python.packages import get_package_share_directory
 
@@ -7,35 +6,23 @@ from launch.substitutions import Command
 from launch_ros.actions import Node
 from launch_ros.descriptions import ParameterValue
 
-#                PathJoinSubstitution([
-#                    FindPackageShare('kobuki_description'),
-#                    'example_substitutions.launch.py'
-#                ])
-
 def generate_launch_description():
     rviz_config = os.path.join(
             get_package_share_directory('kobuki_description'),
             'rviz',
             'model.rviz'
         )
-    path_to_xacro = os.path.join(
+    path_to_urdf = os.path.join(
             get_package_share_directory('kobuki_description'),
             'urdf',
-            'kobuki_standalone.urdf.xacro'
+            'kobuki_standalone.urdf'
         )
-    print("+++++++++++++", rviz_config)
-    print("*************", path_to_xacro)
-    #get_package_share_path('kobuki_description') / 'urdf' / 'kobuki_standalone.urdf.xacro'
     return LaunchDescription([
         Node(
             package='robot_state_publisher',
             executable='robot_state_publisher',
-            parameters=[{
-                'robot_description': ParameterValue(
-                    Command(['xacro', str(path_to_xacro)]),
-                    value_type=str
-                )
-            }]
+            output='screen',
+            arguments=[path_to_urdf,]
         ),
         Node(
             package='joint_state_publisher',
@@ -46,6 +33,6 @@ def generate_launch_description():
             package='rviz2',
             executable='rviz2',
             name='rviz2',
-            #arguments=['-d', rviz_config]
+            arguments=['-d', rviz_config]
         )
     ])
